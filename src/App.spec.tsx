e7b62845f507e0f8a9bb82af80b8ccab31402a31
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2023-01-07 23:35:06 -0300

    adjusting App component

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,12 +1,9 @@
 import { render, screen } from "@testing-library/react"
 import App from "./App"
 
-describe('<App>', ()=> {
+describe('<App />', () => {
   it('renders without crashing', () => {
-    render(
-      <App />
-    )
-
-    expect(screen.getByText('Hello World!')).toBeInTheDocument()
+    render(<App />)
+    expect(screen.getByText('Chessboard')).toBeInTheDocument()
   })
 })
