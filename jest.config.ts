8c1bee34ea6d3d897b3f1b6b4f1de764f5d675ee
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2023-01-07 23:36:28 -0300

    fix jest config to import scss files

--- jest.config.ts
+++ jest.config.ts
@@ -85,7 +85,7 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {"^.+\\.(css|less|scss)$": "babel-jest"},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
