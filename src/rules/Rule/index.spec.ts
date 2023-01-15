787748d695acadf37dbd1d9b4fc2a83c966eb4ea
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2023-01-15 19:19:24 -0300

    update Rule to be a pattern

--- src/rules/Rule/index.spec.ts
+++ src/rules/Rule/index.spec.ts
@@ -1,87 +1,39 @@
-import { INITIAL_BOARD_STATE } from "../../constants/initialBoardState"
-import { PiecePosition } from "../../entities/piecePosition"
-import { PieceType } from "../../entities/pieceType"
-import { Team } from "../../entities/team"
 import { Rule } from "."
+import { INITIAL_BOARD_STATE } from "../../constants/initialBoardState"
+import { Team } from "../../entities/team"
 
 describe('Rule', () => {
-  const rule = new Rule()
+  describe('tileIsOccupied', () => {
+    it('returns true if the tile is occupied', () => {
+      const position = { x: 0, y: 0 }
+      const result = Rule.tileIsOccupied(position, INITIAL_BOARD_STATE)
+      expect(result).toBe(true)
+    })
 
-  describe('isValidMove', () => {
-    describe('when the piece is a pawn', () => {
-      function callWhite(position: PiecePosition, initialBoardState = INITIAL_BOARD_STATE, py = 1) {
-        return rule.isValidMove({ x: 0, y: py }, position, PieceType.PAWN, Team.WHITE, initialBoardState)
-      }
+    it('returns false if the tile is not occupied', () => {
+      const position = { x: 0, y: 2 }
+      const result = Rule.tileIsOccupied(position, INITIAL_BOARD_STATE)
+      expect(result).toBe(false)
+    })
+  })
 
-      function callBlack(position: PiecePosition, initialBoardState = INITIAL_BOARD_STATE, py = 6) {
-        return rule.isValidMove({ x: 0, y: py }, position, PieceType.PAWN, Team.BLACK, initialBoardState)
-      }
+  describe('tileIsOccupiedByEnemy', () => {
+    it('returns true if the tile is occupied by an enemy', () => {
+      const position = { x: 0, y: 0 }
+      const result = Rule.tileIsOccupiedByEnemy(position, INITIAL_BOARD_STATE, Team.BLACK)
+      expect(result).toBe(true)
+    })
 
-      describe('when the pawn moves one square forward', () => {
-        describe('when there is no piece one square forward', () => {
-          it('returns true', () => {
-            expect(callWhite({ x: 0, y: 2 })).toBe(true)
-            expect(callBlack({ x: 0, y: 5 })).toBe(true)
-          })
-        })
-        describe('when there is a piece one square forward', () => {
-          it('returns false', () => {
-            const boardStateWhite = [...INITIAL_BOARD_STATE, { image: '', position: { x: 0, y: 2 }, type: PieceType.PAWN, team: Team.BLACK }]
-            expect(callWhite({ x: 0, y: 2 }, boardStateWhite)).toBe(false)
-            const boardStateBlack = [...INITIAL_BOARD_STATE, { image: '', position: { x: 0, y: 5 }, type: PieceType.PAWN, team: Team.WHITE }]
-            expect(callBlack({ x: 0, y: 5 }, boardStateBlack)).toBe(false)
-          })
-        })
-      })
+    it('returns false if the tile is not occupied', () => {
+      const position = { x: 0, y: 2 }
+      const result = Rule.tileIsOccupiedByEnemy(position, INITIAL_BOARD_STATE, Team.WHITE)
+      expect(result).toBe(false)
+    })
 
-      describe('when the pawn moves two square forward', () => {
-        describe('when piece is in the start position', () => {
-          describe('when there is no piece two square ahead', () => {
-            it('returns true', () => {
-              expect(callWhite({ x: 0, y: 3 })).toBe(true)
-              expect(callBlack({ x: 0, y: 4 })).toBe(true)
-            })
-          })
-          describe('when there is a piece two square ahead', () => {
-            it('returns false', () => {
-              const boardStateWhite = [...INITIAL_BOARD_STATE, { image: '', position: { x: 0, y: 3 }, type: PieceType.PAWN, team: Team.BLACK }]
-              expect(callWhite({ x: 0, y: 3 }, boardStateWhite)).toBe(false)
-              const boardStateBlack = [...INITIAL_BOARD_STATE, { image: '', position: { x: 0, y: 4 }, type: PieceType.PAWN, team: Team.WHITE }]
-              expect(callBlack({ x: 0, y: 4 }, boardStateBlack)).toBe(false)
-            })
-          })
-        })
-        describe('when piece is not in the start position', () => {
-          it('returns false', () => {
-            expect(callWhite({ x: 0, y: 4 }, INITIAL_BOARD_STATE, 2)).toBe(false)
-            expect(callBlack({ x: 0, y: 3 }, INITIAL_BOARD_STATE, 5)).toBe(false)
-          })
-        })
-      })
-
-      describe('when the pawn moves one square diagonally', () => {
-        describe('when there is a piece one square diagonally', () => {
-          it('returns true', () => {
-            const boardStateWhite = [...INITIAL_BOARD_STATE, { image: '', position: { x: 1, y: 2 }, type: PieceType.PAWN, team: Team.BLACK }]
-            expect(callWhite({ x: 1, y: 2 }, boardStateWhite)).toBe(true)
-            const boardStateBlack = [...INITIAL_BOARD_STATE, { image: '', position: { x: 1, y: 5 }, type: PieceType.PAWN, team: Team.WHITE }]
-            expect(callBlack({ x: 1, y: 5 }, boardStateBlack)).toBe(true)
-          })
-        })
-        describe('when there is no piece one square diagonally', () => {
-          it('returns false', () => {
-            expect(callWhite({ x: 1, y: 2 })).toBe(false)
-            expect(callBlack({ x: 1, y: 5 })).toBe(false)
-          })
-        })
-      })
-
-      describe('When piece makes a not allowed move', () => {
-        it('returns false', () => {
-          expect(callWhite({ x: 1, y: 2 })).toBe(false)
-          expect(callBlack({ x: 1, y: 5 })).toBe(false)
-        })
-      })
+    it('returns false if the tile is occupied by a friendly piece', () => {
+      const position = { x: 0, y: 0 }
+      const result = Rule.tileIsOccupiedByEnemy(position, INITIAL_BOARD_STATE, Team.WHITE)
+      expect(result).toBe(false)
     })
   })
 })
--- src/rules/Rule/index.ts
+++ src/rules/Rule/index.ts
@@ -1,41 +1,15 @@
 import { Piece } from "../../entities/piece"
 import { PiecePosition } from "../../entities/piecePosition"
-import { PieceType } from "../../entities/pieceType"
 import { Team } from "../../entities/team"
 import { comparePositions } from "../../helpers/comparePositions"
 
 class Rule {
-  tileIsOccupied(position: PiecePosition, boardState: Piece[]): Boolean {
+  static tileIsOccupied(position: PiecePosition, boardState: Piece[]): Boolean {
     return boardState.find((piece) => comparePositions(piece.position, position)) !== undefined
   }
 
-  tileIsOccupiedByEnemy(position: PiecePosition, boardState: Piece[], team: Team): Boolean {
-    return boardState.find((piece) => comparePositions(piece.position, position)) !== undefined
-  }
-
-  isValidMove(
-    initialPosition: PiecePosition,
-    finalPosition: PiecePosition,
-    type: PieceType,
-    team: Team,
-    boardState: Piece[]
-  ): Boolean {
-
-    if (type === PieceType.PAWN) {
-      const startRow = team === Team.WHITE ? 1 : 6
-      const direction = team === Team.WHITE ? 1 : -1
-
-      if (initialPosition.x === finalPosition.x && (finalPosition.y - initialPosition.y === direction || (finalPosition.y - initialPosition.y === direction * 2 && initialPosition.y === startRow))) {
-        if (finalPosition.y - initialPosition.y === direction * 2) {
-          return !this.tileIsOccupied(finalPosition, boardState) && !this.tileIsOccupied({ x: finalPosition.x, y: finalPosition.y - direction }, boardState)
-        }
-        return !this.tileIsOccupied(finalPosition, boardState)
-      } else if ([1, -1].includes(finalPosition.x - initialPosition.x) && finalPosition.y - initialPosition.y === direction) {
-        return this.tileIsOccupiedByEnemy(finalPosition, boardState, team)
-      }
-    }
-
-    return false
+  static tileIsOccupiedByEnemy(position: PiecePosition, boardState: Piece[], team: Team): Boolean {
+    return boardState.find((piece) => comparePositions(piece.position, position) && piece.team !== team) !== undefined
   }
 }
 
