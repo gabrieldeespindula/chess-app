01b8cd2622f3bea4ad6b347588e359e0429bdf3a
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2023-01-15 19:20:24 -0300

    fix format at comparePositions

--- src/helpers/comparePositions.ts
+++ src/helpers/comparePositions.ts
@@ -1,4 +1,4 @@
-import { PiecePosition } from "../entities/piecePosition";
+import { PiecePosition } from "../entities/piecePosition"
 
 export function comparePositions(firstPosition: PiecePosition, secondPosition: PiecePosition): Boolean{
   return firstPosition.x === secondPosition.x && firstPosition.y === secondPosition.y
