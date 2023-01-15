efabbeab8b263e96e35eb9ba60e83bbaf9918c74
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2023-01-15 18:20:33 -0300

    reafactor referee test and adding atack test

--- src/referee/Referee.spec.ts
+++ src/referee/Referee.spec.ts
@@ -1,4 +1,5 @@
 import { INITIAL_BOARD_STATE } from "../constants/initialBoardState"
+import { PiecePosition } from "../entities/piecePosition"
 import { PieceType } from "../entities/pieceType"
 import { Team } from "../entities/team"
 import { Referee } from "./Referee"
@@ -8,27 +9,27 @@ describe('Referee', () => {
 
   describe('isValidMove', () => {
     describe('when the piece is a pawn', () => {
-      function callWhite(x: number, y: number, initialBoardState = INITIAL_BOARD_STATE, py = 1) {
-        return referee.isValidMove({ x: 0, y: py }, { x, y }, PieceType.PAWN, Team.WHITE, initialBoardState)
+      function callWhite(position: PiecePosition, initialBoardState = INITIAL_BOARD_STATE, py = 1) {
+        return referee.isValidMove({ x: 0, y: py }, position, PieceType.PAWN, Team.WHITE, initialBoardState)
       }
 
-      function callBlack(x: number, y: number, initialBoardState = INITIAL_BOARD_STATE, py = 6) {
-        return referee.isValidMove({ x: 0, y: py }, { x, y }, PieceType.PAWN, Team.BLACK, initialBoardState)
+      function callBlack(position: PiecePosition, initialBoardState = INITIAL_BOARD_STATE, py = 6) {
+        return referee.isValidMove({ x: 0, y: py }, position, PieceType.PAWN, Team.BLACK, initialBoardState)
       }
 
       describe('when the pawn moves one square forward', () => {
         describe('when there is no piece one square forward', () => {
           it('returns true', () => {
-            expect(callWhite(0, 2)).toBe(true)
-            expect(callBlack(0, 5)).toBe(true)
+            expect(callWhite({ x: 0, y: 2 })).toBe(true)
+            expect(callBlack({ x: 0, y: 5 })).toBe(true)
           })
         })
         describe('when there is a piece one square forward', () => {
           it('returns false', () => {
             const boardStateWhite = [...INITIAL_BOARD_STATE, { image: '', position: { x: 0, y: 2 }, type: PieceType.PAWN, team: Team.BLACK }]
-            expect(callWhite(0, 2, boardStateWhite)).toBe(false)
+            expect(callWhite({ x: 0, y: 2 }, boardStateWhite)).toBe(false)
             const boardStateBlack = [...INITIAL_BOARD_STATE, { image: '', position: { x: 0, y: 5 }, type: PieceType.PAWN, team: Team.WHITE }]
-            expect(callBlack(0, 5, boardStateBlack)).toBe(false)
+            expect(callBlack({ x: 0, y: 5 }, boardStateBlack)).toBe(false)
           })
         })
       })
@@ -37,31 +38,48 @@ describe('Referee', () => {
         describe('when piece is in the start position', () => {
           describe('when there is no piece two square ahead', () => {
             it('returns true', () => {
-              expect(callWhite(0, 3)).toBe(true)
-              expect(callBlack(0, 4)).toBe(true)
+              expect(callWhite({ x: 0, y: 3 })).toBe(true)
+              expect(callBlack({ x: 0, y: 4 })).toBe(true)
             })
           })
           describe('when there is a piece two square ahead', () => {
             it('returns false', () => {
               const boardStateWhite = [...INITIAL_BOARD_STATE, { image: '', position: { x: 0, y: 3 }, type: PieceType.PAWN, team: Team.BLACK }]
-              expect(callWhite(0, 3, boardStateWhite)).toBe(false)
+              expect(callWhite({ x: 0, y: 3 }, boardStateWhite)).toBe(false)
               const boardStateBlack = [...INITIAL_BOARD_STATE, { image: '', position: { x: 0, y: 4 }, type: PieceType.PAWN, team: Team.WHITE }]
-              expect(callBlack(0, 4, boardStateBlack)).toBe(false)
+              expect(callBlack({ x: 0, y: 4 }, boardStateBlack)).toBe(false)
             })
           })
         })
         describe('when piece is not in the start position', () => {
           it('returns false', () => {
-            expect(callWhite(0, 4, INITIAL_BOARD_STATE, 2)).toBe(false)
-            expect(callBlack(0, 3, INITIAL_BOARD_STATE, 5)).toBe(false)
+            expect(callWhite({ x: 0, y: 4 }, INITIAL_BOARD_STATE, 2)).toBe(false)
+            expect(callBlack({ x: 0, y: 3 }, INITIAL_BOARD_STATE, 5)).toBe(false)
+          })
+        })
+      })
+
+      describe('when the pawn moves one square diagonally', () => {
+        describe('when there is a piece one square diagonally', () => {
+          it('returns true', () => {
+            const boardStateWhite = [...INITIAL_BOARD_STATE, { image: '', position: { x: 1, y: 2 }, type: PieceType.PAWN, team: Team.BLACK }]
+            expect(callWhite({ x: 1, y: 2 }, boardStateWhite)).toBe(true)
+            const boardStateBlack = [...INITIAL_BOARD_STATE, { image: '', position: { x: 1, y: 5 }, type: PieceType.PAWN, team: Team.WHITE }]
+            expect(callBlack({ x: 1, y: 5 }, boardStateBlack)).toBe(true)
+          })
+        })
+        describe('when there is no piece one square diagonally', () => {
+          it('returns false', () => {
+            expect(callWhite({ x: 1, y: 2 })).toBe(false)
+            expect(callBlack({ x: 1, y: 5 })).toBe(false)
           })
         })
       })
 
       describe('When piece makes a not allowed move', () => {
         it('returns false', () => {
-          expect(callWhite(1, 2)).toBe(false)
-          expect(callBlack(1, 5)).toBe(false)
+          expect(callWhite({ x: 1, y: 2 })).toBe(false)
+          expect(callBlack({ x: 1, y: 5 })).toBe(false)
         })
       })
     })
--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -5,11 +5,11 @@ import { Team } from "../entities/team"
 import { comparePositions } from "../helpers/comparePositions"
 
 class Referee {
-  tileIsOccupied(position: PiecePosition, boardState: Piece[]): Boolean{
-    return boardState.find((piece) => (piece.position, position)) !== undefined
+  tileIsOccupied(position: PiecePosition, boardState: Piece[]): Boolean {
+    return boardState.find((piece) => comparePositions(piece.position, position)) !== undefined
   }
 
-  tileIsOccupiedByEnemy(position: PiecePosition, boardState: Piece[], team: Team): Boolean{
+  tileIsOccupiedByEnemy(position: PiecePosition, boardState: Piece[], team: Team): Boolean {
     return boardState.find((piece) => comparePositions(piece.position, position)) !== undefined
   }
 
@@ -19,18 +19,18 @@ class Referee {
     type: PieceType,
     team: Team,
     boardState: Piece[]
-  ): Boolean{
+  ): Boolean {
 
-    if(type === PieceType.PAWN){
+    if (type === PieceType.PAWN) {
       const startRow = team === Team.WHITE ? 1 : 6
       const direction = team === Team.WHITE ? 1 : -1
 
-      if(initialPosition.x === finalPosition.x && (finalPosition.y - initialPosition.y === direction || (finalPosition.y - initialPosition.y === direction * 2 && initialPosition.y === startRow))){
-        if(finalPosition.y - initialPosition.y === direction * 2){
-          return !this.tileIsOccupied(finalPosition, boardState) && !this.tileIsOccupied({ x: finalPosition.x, y: finalPosition.y - direction}, boardState)
+      if (initialPosition.x === finalPosition.x && (finalPosition.y - initialPosition.y === direction || (finalPosition.y - initialPosition.y === direction * 2 && initialPosition.y === startRow))) {
+        if (finalPosition.y - initialPosition.y === direction * 2) {
+          return !this.tileIsOccupied(finalPosition, boardState) && !this.tileIsOccupied({ x: finalPosition.x, y: finalPosition.y - direction }, boardState)
         }
         return !this.tileIsOccupied(finalPosition, boardState)
-      } else if([1, -1].includes(finalPosition.x - initialPosition.x) && finalPosition.y - initialPosition.y === direction){
+      } else if ([1, -1].includes(finalPosition.x - initialPosition.x) && finalPosition.y - initialPosition.y === direction) {
         return this.tileIsOccupiedByEnemy(finalPosition, boardState, team)
       }
     }
