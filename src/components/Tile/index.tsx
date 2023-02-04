bd214c4a40aac21441752d0cbeb4c91ce88bf077
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2023-02-04 14:44:33 -0300

    create highlight

--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -3,13 +3,16 @@ import './index.scss'
 type Props = {
   number: number
   image?: string
+  highlight?: boolean
 }
 
-function Tile({ number, image }: Props): JSX.Element {
+function Tile({ number, image, highlight }: Props): JSX.Element {
   const isEven = number % 2 === 0
 
+  const highlightClass = highlight && image ? 'highlight-kill' : highlight ? 'highlight' : ''
+
   return (
-    <div className={`tile ${isEven ? 'white-tile' : 'black-tile'}`}>
+    <div className={`tile ${isEven ? 'white-tile' : 'black-tile'} ${highlightClass}`}>
       {image && <div style={{ backgroundImage: `url(${image})` }} className="piece"></div>}
     </div>
   )
